6a99c8fdc2b7c0afd2f2cc0c61556a3b7565457e
Eliajah Shepard <Eshepard@pgh.yearup.org>
2022-11-06 10:39:20 -0500

    add list of objects and arrays, declare variables for select menu

---
+++ foodMenu.js
@@ -0,0 +1,38 @@
+
+window.onselect = function (){
+
+
+let menu = {
+    drinks : [
+     "Water", "Tea", "Sweet Tea", 
+     "Coke", "Dr. Pepper", "Sprite"
+     ],
+    entrees : [
+    "Hamburger w/ Fries", 
+     "Grilled Cheese w/ Tater Tots",
+    "Grilled Chicken w/ Veggies", 
+     "Chicken Fried Steak w/ Mashed Potatoes",
+    "Fried Shrimp w/ Coleslaw", 
+     "Veggie Plate"
+    ],
+    desserts: [
+     "Cheesecake", "Chocolate Cake", "Snickerdoodle Cookie"
+     ]
+    };
+
+
+
+
+
+    let selectMenu = document.getElementById('menu');
+    console.log(`the select menu is ${selectMenu}`);
+    let moreInfoBtn = document.getElementById('moreInfo');
+    console.log(moreInfoBtn);
+
+
+
+
+
+
+   
+}
